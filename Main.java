0520d600b895efd8eee8b71628689380785e3e61
Chris Koch <kochcc25@uwgb.edu>
2020-06-18 16:13:19 +0000

    Paper

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
-// Pie
-// Pie
-// Pie
-// Pie
-// Pie
-// Pie
-// Pie
-// Pie
-// Pie
-// Pie
-//Programmer has cuncushuns
+
+
+
+
+
+
+
+
+
+
+//Programmer is cuncushuns
 import java.util.*;
 
 public class Main {
@@ -27,7 +27,53 @@ System.out.println("\n\n\n\n");
 
 System.out.println("You Entered:" + something);
 
-System.out.println("I hope that's what you wanted!  It worked!!!!!!!!!!");
+System.out.println("I hope that's what you wanted!  It worked!!!!!!!!!!\n\n\n\n\n");
+
+System.out.println("Rabies surveillance in the Russian Federation\n");
+
+System.out.println("M.I. Shulpin*, N.A. Nazarov, S.A. Chupin, F.I. Korennoy, A.Ye. Metlin & A.V. Mischenko\n\n");
+
+System.out.println("In the Russian Federation, rabies surveillance focuses on\n");
+
+System.out.println("the long-term monitoring of rabies-susceptible animals to\n");
+
+System.out.println("detect the disease, determine its prevalence and trace its\n");
+
+System.out.println("evolution or, alternatively, to confirm its absence in disease-\n");
+
+System.out.println("free areas. This surveillance strategy is the basis for risk\n");
+
+System.out.println("analysis that is intended to protect both animal and human\n");
+
+System.out.println("health, as well as to justify the implementation of veterinary\n");
+
+System.out.println("and disease control measures.\n\n");
+
+System.out.println("Rabies is enzootic in the Russian Federation. Therefore,\n");
+
+System.out.println("disease surveillance is aimed at determining those animal\n");
+
+System.out.println("populations that demonstrate the highest disease prevalence\n");
+
+System.out.println("(i.e. identifying categories of animals in which the rabies\n");
+
+System.out.println("virus [RABV] has been detected). Reliable identification,\n");
+
+System.out.println("notification and classification of such animal populations\n");
+
+System.out.println("depend upon continuous implementation of rabies\n");
+
+System.out.println("awareness programmes for veterinarians.\n\n");
+
+System.out.println("This paper presents data describing the enzootic situation\n");
+
+System.out.println("of rabies in the Russian Federation between 2012 and\n");
+
+System.out.println("2016. The authors also analyse disease incidence in wild\n");
+
+System.out.println("and domestic animals, as well as evaluating preventive\n");
+
+System.out.println("measures.\n");
 
 }
 }
