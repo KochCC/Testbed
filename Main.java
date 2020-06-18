3e01988f654e4ea5984641d25ed561a31e46238b
Chris Koch <kochcc25@uwgb.edu>
2020-06-18 15:14:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+// Pie
+// Pie
+// Pie
+// Pie
+// Pie
+// Pie
+// Pie
+// Pie
+// Pie
+// Pie
+import java.util.*;
+
+public class Main {
+
+public static void main(String[] args) {
+
+Scanner user_input = new Scanner(System.in);
+
+System.out.println("Main\n\n");
+
+System.out.println("Enter Something:");
+
+String something = user_input.nextLine();
+
+System.out.println("\n\n\n\n");
+
+System.out.println("You Entered:" + something);
+
+System.out.println("I hope that's what you wanted!  It worked!!!!!!!!!!");
+
+}
+}
