a4f5453b25b556b5b8e27769c4b8d89f9f50c037
Chris Koch <kochcc25@uwgb.edu>
2020-07-13 16:31:07 +0000

    Trying to upload to github

--- Main.java
+++ Main.java
@@ -1,79 +1,30 @@
-
-
-
-
-
-
-
-
-
-
-//Programmer is cuncushuns
+//"Inspired" by Monty Python's Search for the Holy Grail movie, Bridge of Death scene
 import java.util.*;
-
 public class Main {
-
 public static void main(String[] args) {
+Scanner inputword = new Scanner(System.in);
 
-Scanner user_input = new Scanner(System.in);
+System.out.println("YOU HAVE OPENED UP THE BRIDGE...I mean...PROGRAM OF DEATH! \n");
+System.out.println("What is your name?");
+String userName = inputword.nextLine();
 
-System.out.println("Main\n\n");
+System.out.println("\nWhat is your quest?");
+String thisisthequest = inputword.nextLine();
 
-System.out.println("Enter Something:");
+System.out.println("\nWhat is the capital of Assyria?");
+String oneofafewcities = inputword.nextLine();
 
-String something = user_input.nextLine();
+System.out.println("\nWhat is the airspeed of the unlaiden European swallow in meters per second?");
+Double europeanswallows = inputword.nextDouble();  //I am not sure how to do a test to see if a number was typed yet
 
-System.out.println("\n\n\n\n");
+System.out.println("\nWhat is the airspeed of the unlaiden African swallow in meters per second?");
+Double africanswallows = inputword.nextDouble();  //I am not sure how to do a test to see if a number was typed yet
 
-System.out.println("You Entered:" + something);
-
-System.out.println("I hope that's what you wanted!  It worked!!!!!!!!!!\n\n\n\n\n");
-
-System.out.println("Rabies surveillance in the Russian Federation\n");
-
-System.out.println("M.I. Shulpin*, N.A. Nazarov, S.A. Chupin, F.I. Korennoy, A.Ye. Metlin & A.V. Mischenko\n\n");
-
-System.out.println("In the Russian Federation, rabies surveillance focuses on\n");
-
-System.out.println("the long-term monitoring of rabies-susceptible animals to\n");
-
-System.out.println("detect the disease, determine its prevalence and trace its\n");
-
-System.out.println("evolution or, alternatively, to confirm its absence in disease-\n");
-
-System.out.println("free areas. This surveillance strategy is the basis for risk\n");
-
-System.out.println("analysis that is intended to protect both animal and human\n");
-
-System.out.println("health, as well as to justify the implementation of veterinary\n");
-
-System.out.println("and disease control measures.\n\n");
-
-System.out.println("Rabies is enzootic in the Russian Federation. Therefore,\n");
-
-System.out.println("disease surveillance is aimed at determining those animal\n");
-
-System.out.println("populations that demonstrate the highest disease prevalence\n");
-
-System.out.println("(i.e. identifying categories of animals in which the rabies\n");
-
-System.out.println("virus [RABV] has been detected). Reliable identification,\n");
-
-System.out.println("notification and classification of such animal populations\n");
-
-System.out.println("depend upon continuous implementation of rabies\n");
-
-System.out.println("awareness programmes for veterinarians.\n\n");
-
-System.out.println("This paper presents data describing the enzootic situation\n");
-
-System.out.println("of rabies in the Russian Federation between 2012 and\n");
-
-System.out.println("2016. The authors also analyse disease incidence in wild\n");
-
-System.out.println("and domestic animals, as well as evaluating preventive\n");
-
-System.out.println("measures.\n");
+System.out.println("\n\n\n\n\n\nSir " + userName + ", trying to " + thisisthequest + ",");
+System.out.println("Who does not realize " + oneofafewcities + " is the wrong way to spell the capitol of Assyria,");
+System.out.println("who assumes " + europeanswallows + "is the airspeed of the unlaiden European swallow,");
+System.out.println("and that the unlaiden airspeed of the African swallow is" + africanswallows + ",");
+System.out.println("has the program's fullest permission to do the thing!");
 
 }
 }
